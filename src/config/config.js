0795f9b82b23f936ed17dbc272fb630e6f359f9f
Mulelur <63222158+Mulelur@users.noreply.github.com>
2022-04-29 22:10:26 +0200

    epo

---
+++ src/config/config.js
@@ -0,0 +1,162 @@
+export const ProjectDetails = {
+  projectName: "epo-e",
+  projectVersion: "1.0.0.2",
+  projectDescription: "",
+  projectAuthor: "",
+  projectAuthorEmail: "",
+  projectAuthorUrl: "",
+  projectLicense: "",
+  projectLicenseUrl: "",
+  projectCopyright: "",
+  projectHomepage: "",
+  projectRepository: "",
+  projectRepositoryType: "",
+  projectRepositoryUrl: "",
+  projectRepositoryBranch: "",
+};
+
+export const user = {
+  name: "KURHULA PEACW THEMBA",
+  email: "kurhulathemba183@gmail.com",
+  phoneNumber: "072 198 0100",
+  address: "Lulekanin Stand no 498 Phalaborwa 1390",
+  password: "",
+  confirmPassword: "",
+  role: "",
+  status: "",
+  degree: "",
+  createdAt: "",
+  updatedAt: "",
+};
+
+export const homePage = {
+  logoName: "KURHULA",
+  intro: "I'm a future engineer",
+  profile: {
+    url: "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    desc: "Home profile picture",
+  },
+  quote:
+    "Productivity is never an accident. It’s always the result of a commitment to excellence, intelligent planning, and focused effort.",
+  text: "I am taking new projects. Waiting to hearing about new projects, so if you'd like to chat please get in touch.",
+  links: [
+    {
+      name: "linkedin",
+      link: "",
+    },
+  ],
+};
+
+export const aboutPage = {
+  intro:
+    "I AM A KRHULA PEACE THEMBA (A FUTURE ENGINEER). I AM A STUDENT AT VAAL UNIVERSITY OF TECHNOLOGY STUDYING ",
+  names: "KURHULA PEACE THEMBA",
+  profile: {
+    url: "https://images.unsplash.com/photo-1523824921871-d6f1a15151f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    desc: '"I am a future engineer"',
+  },
+  summary:
+    "I AM KURHULA PEACE THEMBA , I AM A STUDENT AT VAAL UNIVERSITY OF TECHNOLOGY . I AM DOING MECHANICAL ENGINEERING , I CHOSE TO DO THIS COURSE BECAUSE I LOVE  TO DESIGN AND BUILD SOLUTIONS TO A RANGE OF PROBLEMS.I WOULD LOVE TO BE ONE OF BEST MECHANICAL ENGINEERS IN THE WORLD WHO IMPROVES EFFICIENCY ACROSS A WIDE RANGE OF INDUSTRIES. I LOVE READING BOOKS, PLAYING NETBALL, AND GOING OUT WITH MY FRIENDS FOR A VACATION,",
+  LearningStyles: [
+    {
+      title: " Logical (Mathematical)",
+      desc: "I you like using my brain for logical and mathematical reasoning,i am a logical learner",
+    },
+    {
+      title: "Aural (Auditory-Musical)",
+      desc: "I need someone to tell me something out loud to understand it, i am, an auditory learner",
+    },
+    {
+      title: " Verbal (Linguistic)",
+      desc: "I find it easier to express myself by writing or speaking i regard it as verbal learners. i love to write and read",
+    },
+
+    {
+      title: "Physical (Kinesthetic)",
+      desc: "In this style, learning happens when the learner carries out a physical activity, rather than listening to a lecture or watching a demonstration",
+    },
+  ],
+  links: [
+    {
+      title: "Download CV",
+      link: "",
+    },
+    {
+      title: "Download Letter",
+      link: "",
+    },
+  ],
+};
+
+export const hobbiesPage = {
+  intro: "Most of us have free time, we just don’t spend it wisely.",
+  hobbies: [
+    {
+      title: "01. Reading",
+      desc: "I READ DIFFERENT KIND OF BOOKS BUT BECAUSE AM STILL  A STUDENT WHO IS DOING A CHALLENGING COURSE I TEND TO READ MY EDUCATION BOOKS",
+    },
+    {
+      title: "02.PLAYING NETBALL",
+      desc: " I PLAY NETBALL WITH MY FRIENDS WHENEVER AM TIRED OF READING FUNNY HOW THIS TURNED INTO A HOBBY TO ME",
+    },
+    {
+      title: "03.VACATIONS",
+      desc: "I MAKE SURE EVERY MONTHS ENG I GO OUT FOR A VACATION EITHER WITH MY FRIENDS OR SIBLINGS.",
+    },
+    {
+      title: "04. SHOPPING",
+      desc: "SHOPPING IS MY THING AND I REALLY LOVE IT WHENEVER I FEEL LIKE I HAVE MONEY I JUST GO FOR A SHOPPING FUNNY HOW THIS TURNED INTO ONE OF MY HOBBIES",
+    },
+  ],
+  topImgs: [
+    {
+      url: "https://images.unsplash.com/photo-1533647046292-0187d1d2c978?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1567629733542-916a5af7e72a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+  ],
+  bottomImgs: [
+    {
+      url: "https://images.unsplash.com/photo-1619194999378-762462c38ab4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3027&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1600165642369-f609e6a2daa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2992&q=80",
+    },
+  ],
+};
+
+export const projectsPage = {
+  intro: "Most of us have free time, we just don’t spend it wisely.",
+  projects: [
+    {
+      title: "A new project",
+      link: "",
+      module: "Appl com",
+    },
+    {
+      title: "A new project",
+      link: "",
+      module: "Appl com",
+    },
+  ],
+  highSchoolSubjects: [
+    "Xitsonga Home Language",
+    "English first additional language",
+    "Mathematics",
+    "Life orientation",
+    "Agricultural Sciences",
+    "Life Sciences",
+    "Physical Sciences",
+    "Religious Studies",
+  ],
+  modules: [
+    "Engineering Skills",
+    "Social Intelligence",
+    "Mathematics 1",
+    "Physics 1",
+    "Engineering Chemistry 1",
+    "ICT Skills",
+    "Applied Communication skills",
+  ],
+};
