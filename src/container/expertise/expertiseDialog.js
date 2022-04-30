627da78f5db37aed2050671067544fe50b21e9b3
Mulelur <63222158+Mulelur@users.noreply.github.com>
2022-04-30 09:54:13 +0200

    .

--- src/container/expertise/expertiseDialog.js
+++ src/container/expertise/expertiseDialog.js
@@ -56,12 +56,7 @@ export default function ExpertiseDialog() {
                       <>
                         <Expertise.Col key={index}>
                           <Expertise.ScreenBox>
-                            <Expertise.Link href={item.link}>
-                              <Expertise.Img
-                                alt="img"
-                                src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                              />
-                            </Expertise.Link>
+                            <Expertise.Link href={item.link}></Expertise.Link>
                             <Expertise.TextBox>
                               <Expertise.TextBoxText>
                                 {item.module}
